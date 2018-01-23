feaf6b23e624573d0b4d4ecd56e281f5a7e5972e
Ben Lee <leebenjamin@utexas.edu>
2018-01-22 18:08:13 -0600

    add error check max char len

--- src/Post.java
+++ src/Post.java
@@ -4,28 +4,31 @@ import twitter4j.TwitterFactory;
 
 public class Post {
 
-    //static final Twitter twitterInstance = TwitterFactory.getSingleton();
+    static final Twitter twitterInstance = TwitterFactory.getSingleton();
+    static final int MAX_CHAR_LIMIT = 280;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         if (args.length != 1) {
             System.out.println("Incorrect number of arguments. Usage: java Post [tweet in quotes]");
             return;
         }
 
         String tweet = args[0];
+        updateStatus(tweet);
         System.out.println(tweet);
     }
 
     public static boolean validateTweet(String tweet) {
+        if (tweet.length() > MAX_CHAR_LIMIT) return false;
         return true;
     }
 
-    public static String updateStatus(String tweet) {
+    public static String updateStatus(String tweet) throws TwitterException {
         if (validateTweet(tweet)) {
-            //twitterInstance.updateStatus(tweet);
+            twitterInstance.updateStatus(tweet);
             return tweet;
         }
-        return "Malformed tweet.";
+        return "Malformed tweet, maximum character length is 280 characters.";
     }
 
 }
