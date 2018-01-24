9e3b473141b44e96cdf1f4770b501c321408ac42
Ben Lee <leebenjamin@utexas.edu>
2018-01-24 11:17:07 -0600

    reverting Post

--- src/Post.java
+++ src/Post.java
@@ -4,15 +4,17 @@ import twitter4j.TwitterFactory;
 
 public class Post {
 
-    //static final Twitter twitterInstance = TwitterFactory.getSingleton();
+    static final Twitter twitterInstance = TwitterFactory.getSingleton();
+    static final int MAX_CHAR_LIMIT = 280;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         if (args.length != 1) {
             System.out.println("Incorrect number of arguments. Usage: java Post [tweet in quotes]");
             return;
         }
 
         String tweet = args[0];
+        updateStatus(tweet);
         System.out.println(tweet);
     }
 
@@ -20,12 +22,12 @@ public class Post {
         return (tweet.length() > MAX_CHAR_LIMIT);
     }
 
-    public static String updateStatus(String tweet) {
+    public static String updateStatus(String tweet) throws TwitterException {
         if (validateTweet(tweet)) {
-            //twitterInstance.updateStatus(tweet);
+            twitterInstance.updateStatus(tweet);
             return tweet;
         }
-        return "Malformed tweet.";
+        return "Malformed tweet, maximum character length is 280 characters.";
     }
 
 }
