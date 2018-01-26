161c5916087ecda0a5e8463a52e160e29a058890
Ben Lee <leebenjamin@utexas.edu>
2018-01-26 16:25:13 -0600

    remove unnecessary configurations left over from helloworld

--- src/main/java/com/benjamin/benleethium/BenLeethiumConfiguration.java
+++ src/main/java/com/benjamin/benleethium/BenLeethiumConfiguration.java
@@ -8,9 +8,6 @@ public class BenLeethiumConfiguration extends Configuration {
     @NotEmpty
     private String template;
 
-    @NotEmpty
-    private String defaultName = "Stranger";
-
     @JsonProperty
     public String getTemplate() {
         return template;
@@ -19,16 +16,6 @@ public class BenLeethiumConfiguration extends Configuration {
     @JsonProperty
     public void setTemplate(String template) {
         this.template = template;
-    }
-
-    @JsonProperty
-    public String getDefaultName() {
-        return defaultName;
-    }
-
-    @JsonProperty
-    public void setDefaultName(String name) {
-        this.defaultName = name;
     }
 }
   
