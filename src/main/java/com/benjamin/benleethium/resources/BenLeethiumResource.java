7efe41785c60c5edef70b7c13d558c195c695c86
Ben Lee <leebenjamin@utexas.edu>
2018-01-30 16:57:06 -0600

    move Response logic to Resource for Post

--- src/main/java/com/benjamin/benleethium/resources/BenLeethiumResource.java
+++ src/main/java/com/benjamin/benleethium/resources/BenLeethiumResource.java
@@ -1,9 +1,15 @@
 package com.benjamin.benleethium.resources;
 
+import com.benjamin.benleethium.api.GetResponse;
+import com.benjamin.benleethium.api.ErrorResponse;
 import com.benjamin.benleethium.services.Get;
 import com.benjamin.benleethium.services.Post;
 import com.codahale.metrics.annotation.Timed;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -14,17 +20,28 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import twitter4j.TwitterException;
+
 @Path("/api/1.0/twitter")
 @Produces(MediaType.APPLICATION_JSON)
 public class BenLeethiumResource {
 
+    final Logger logger = LoggerFactory.getLogger(BenLeethiumResource.class);
     public BenLeethiumResource() {}
 
     @GET
     @Path("/timeline")
     @Timed
     public Response getHomeTimeline() {
-        return Get.getInstance().getHomeTimeline();
+        try {
+            List<String> homelineTweets = Get.getInstance().getHomeTimeline();
+            return Response.ok(new GetResponse(homelineTweets)).build();
+        } catch (TwitterException e) {
+            logger.warn("Unable to fetch home timeline.", e);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(new ErrorResponse("There is a problem with the server. Please try again later"))
+                .build();
+        }
     }
 
     @POST
--- src/main/java/com/benjamin/benleethium/services/Get.java
+++ src/main/java/com/benjamin/benleethium/services/Get.java
@@ -1,7 +1,5 @@
 package com.benjamin.benleethium.services;
 
-import com.benjamin.benleethium.api.GetResponse;
-import com.benjamin.benleethium.api.ErrorResponse;
 import com.benjamin.benleethium.BenLeethiumApplication;
 
 import org.slf4j.Logger;
@@ -13,7 +11,6 @@ import twitter4j.TwitterException;
 
 import java.util.ArrayList;
 import java.util.List;
-import javax.ws.rs.core.Response;
 
 public class Get {
 
@@ -29,23 +26,16 @@ public class Get {
         return INSTANCE;
     }
 
-    public Response getHomeTimeline() {
-        try {
-            logger.debug("Retrieving home timeline...");
-            List<Status> statuses = twitterInstance.getHomeTimeline();
+    public List<String> getHomeTimeline() throws TwitterException {
+        logger.debug("Retrieving home timeline...");
+        List<Status> statuses = twitterInstance.getHomeTimeline();
 
-            List<String> homelineTweets = new ArrayList<>();
-            for (Status status : statuses) {
-                homelineTweets.add(status.getUser().getName() + ": " +
-                                   status.getText());
-            }
-            logger.debug("Got home timeline: {}", homelineTweets);
-            return Response.ok(new GetResponse(homelineTweets)).build();
-        } catch (TwitterException e) {
-            logger.warn("Unable to fetch home timeline.", e);
-            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
-                .entity(new ErrorResponse("There is a problem with the server. Please try again later"))
-                .build();
+        List<String> homelineTweets = new ArrayList<>();
+        for (Status status : statuses) {
+            homelineTweets.add(status.getUser().getName() + ": " +
+                               status.getText());
         }
+        logger.debug("Got home timeline: {}", homelineTweets);
+        return homelineTweets;
     }
 }
