64f857d7a68fdc588e3802a03bfbfbd20763d364
Ben Lee <leebenjamin@utexas.edu>
2018-02-02 13:43:47 -0600

    add skeleton tests for TwitterService

--- src/test/java/TwitterServiceTest.java
+++ src/test/java/TwitterServiceTest.java
@@ -1,9 +1,27 @@
 import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+
+import com.benjamin.benleethium.services.TwitterService;
 
 public class TwitterServiceTest {
 
     @Test
-    void example() {
-        System.out.println("Test");
+    void testGetInstance() {
+    }
+
+    @Test
+    void testValidateTweet() {
+    }
+
+    @Test
+    void testUpdateStatus() {
+    }
+
+    @Test
+    void testGetHomeTimeline() {
+    }
+
+    @Test
+    void searchHomeTimeline() {
     }
 }
