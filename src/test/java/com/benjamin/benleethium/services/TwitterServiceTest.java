7651f7134180d70e0fa6bee67ceb9b9c8a837080
Ben Lee <leebenjamin@utexas.edu>
2018-02-02 16:19:31 -0600

    move TwitterServiceTest to matching package structure

--- src/test/java/com/benjamin/benleethium/services/TwitterServiceTest.java
+++ src/test/java/com/benjamin/benleethium/services/TwitterServiceTest.java
@@ -3,10 +3,22 @@ import org.junit.Assert;
 
 import com.benjamin.benleethium.services.TwitterService;
 
+import twitter4j.Twitter;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TwitterServiceTest {
+
+    @Mock
+    Twitter twitterInstance;
+
+    @InjectMocks
+    TwitterService twitterServiceInstance;
 
     @Test
     void testGetInstance() {
+        twitterServiceInstance.getInstance();
+
+
     }
 
     @Test
