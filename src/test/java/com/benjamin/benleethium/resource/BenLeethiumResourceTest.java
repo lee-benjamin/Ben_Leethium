7718310e4bcc168b39971e688c5c718c15de420b
Ben Lee <leebenjamin@utexas.edu>
2018-02-06 16:55:53 -0600

    add skeleton tests for BenLeethiumResourceTest

---
+++ src/test/java/com/benjamin/benleethium/resource/BenLeethiumResourceTest.java
@@ -0,0 +1,36 @@
+package com.benjamin.benleethium.resource;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.benjamin.benleethium.models.Status;
+import com.benjamin.benleethium.models.User;
+import com.benjamin.benleethium.models.StatusFixture;
+import com.benjamin.benleethium.models.UserFixture;
+import com.benjamin.benleethium.models.ResponseListFixture;
+
+import java.util.Date;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mockito.Mockito;
+
+
+public class BenLeethiumResourceTest {
+
+    @Test
+    public void testGetHomeTimeline(){}
+
+    @Test
+    public void testUpdateStatus(){}
+
+    @Test
+    public void testSearchHomeTimeline() {}
+   
+}
+
