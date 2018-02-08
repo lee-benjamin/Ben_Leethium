2e7d85083875a57411e427530f9c64a39c373b85
Ben Lee <leebenjamin@utexas.edu>
2018-02-08 15:48:40 -0600

    add setters to twitterconf for testing purposes

--- src/main/java/com/benjamin/benleethium/Twitter4jConfiguration.java
+++ src/main/java/com/benjamin/benleethium/Twitter4jConfiguration.java
@@ -25,15 +25,30 @@ public class Twitter4jConfiguration {
         return consumerKey;
     }
 
+    public void setConsumerKey(String consumerKey) {
+        this.consumerKey = consumerKey;
+    }
+
     public String getConsumerSecret() {
         return consumerSecret;
     }
 
+    public void setConsumerSecret(String consumerSecret) {
+        this.consumerSecret = consumerSecret;
+    }
     public String getAccessToken() {
         return accessToken;
     }
 
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
     public String getAccessTokenSecret() {
         return accessTokenSecret;
+    }
+
+    public void setAccessTokenSecret(String accessTokenSecret) {
+        this.accessTokenSecret = accessTokenSecret;
     }
 }
