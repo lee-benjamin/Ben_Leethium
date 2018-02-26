3d88fe8c975d514528d25c2c0e10e9760393d023
Ben Lee <leebenjamin@utexas.edu>
2018-02-26 15:39:26 -0600

    add debug mocked code

--- src/main/java/com/benjamin/benleethium/services/TwitterService.java
+++ src/main/java/com/benjamin/benleethium/services/TwitterService.java
@@ -1,6 +1,7 @@
 package com.benjamin.benleethium.services;
 
 import com.benjamin.benleethium.models.Status;
+import com.benjamin.benleethium.models.User;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,6 +9,8 @@ import org.slf4j.LoggerFactory;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.NoSuchElementException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -17,6 +20,7 @@ import javax.inject.Inject;
 
 public class TwitterService {
 
+    private static final boolean DEBUG = true;
     public static final int MAX_CHAR_LIMIT = 280;
     private final Logger logger = LoggerFactory.getLogger(TwitterService.class);
     private Twitter twitterInstance;
@@ -44,6 +48,8 @@ public class TwitterService {
     }
 
     public List<Status> getHomeTimeline() throws TwitterException {
+        if (DEBUG) return generateMockedData();
+
         logger.debug("Retrieving home timeline...");
         return twitterInstance.getHomeTimeline().stream()
                                                 .map(s -> new Status(s))
@@ -56,5 +62,16 @@ public class TwitterService {
                                                 .filter(s -> s.getText().contains(keyword))
                                                 .map(s -> new Status(s))
                                                 .collect(Collectors.toList());
+    }
+
+    public static List<Status> generateMockedData() {
+        List<Status> statuses = new ArrayList<>();
+        String body = "Do not panic, this is only a test.";
+        String id = "968228854388183041";
+        User user = new User("Benjamin Lee", "Ben_Leethium", "http://pbs.twimg.com/profile_images/956243766146252800/rZY43NJM_normal.jpg");
+        for (int i=0;i<15;i++) {
+            statuses.add(new Status(body, new Date(), id, user));
+        }
+        return statuses;
     }
 }
